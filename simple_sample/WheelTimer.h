2dbaa6c707906830457a2c25d28c289f289bf262
Benyamin <beneslami72@gmail.com>
2020-06-24 01:24:56 +0430

    timer implementation - initial

---
+++ simple_sample/WheelTimer.h
@@ -0,0 +1,27 @@
+#ifndef WHEELTIMER_H
+#define WHEELTIMER_H
+
+typedef struct _wheel_timer_ wheel_timer_t;           /* opage structure */
+typedef struct _wheel_timer_elem_ wheel_timer_elem_t; /* opage structure */
+typedef void (*app_call_back)(void *arg, int sizeof_arg);
+
+wheel_timer_t*
+init_wheel_timer(int, int);
+void
+start_wheel_timer(wheel_timer_t*);
+wheel_timer_elem_t*
+register_app_event(wheel_timer_t*, app_call_back , void*, int ,int , char);
+void
+de_register_app_event(wheel_timer_elem_t*, wheel_timer_elem_t*);
+void
+wt_elem_reschedule(wheel_timer_elem_t*, int);
+void
+free_wheel_timer_element(wheel_timer_elem_t*);
+void
+print_wheel_timer(wheel_timer_t*);
+void
+cancel_wheel_timer(wheel_timer_t*);
+void
+reset_wheel_timer(wheel_timer_t*);
+
+#endif
