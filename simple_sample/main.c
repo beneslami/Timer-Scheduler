002001057753e9ebf0d2b537b6d0e39b946953a1
Benyamin <beneslami72@gmail.com>
2020-06-24 19:14:54 +0430

    target sample

--- simple_sample/main.c
+++ simple_sample/main.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+#include <string.h>
 #include "WheelTimer.h"
 #include "LinkedListApi.h"
 
+void
+print(char *str){
+    printf("%s\n", str);
+}
+void wrapper_print(void *arg, int arg_size){
+  char *str = (char*)arg;
+  print(str);
+}
+
 int
 main(int argc, char **argv){
-  wheel_timer_t *wt = init_wheel_timer(/*wheel size, wheel timer clock tic interval -> by Macro  */);
+  wheel_timer_t *wt = init_wheel_timer(5, 1); //10 -> the number of slots , 1->time it takes between each slot
   start_wheel_timer(wt); /* Wheel timer has started running in a separte thread. */
 
   /* Register the events to be triggered with Wheel timer now. */
-  wheel_timer_elem_t *wt_elem = register_app_event(wt,
-  /*wrapper function*/, /*input*/, /*input length*/,
-  /*period */, 1 /*(infinite)*/);
+  wheel_timer_elem_t *wt_elem = register_app_event(wt, wrapper_print, "ben_event", strlen("ben_event"), 3, 1);
 
   scanf("\n");
   return 0;
