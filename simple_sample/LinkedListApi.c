595e5378a7da756a3d8f6200c48009fa59bd8e2a
Benyamin <beneslami72@gmail.com>
2020-06-25 17:27:03 +0430

    add/remove/delete functions + Macro

--- simple_sample/LinkedListApi.c
+++ simple_sample/LinkedListApi.c
@@ -1,6 +1,7 @@
 #include "LinkedListApi.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 struct _ll_node { /* ll_node */
   void* data;
@@ -20,7 +21,7 @@ init_singly_ll(){
   return head;
 }
 
-int 
+int
 add_node_by_val(ll_t *ll , void *data){
   ll_node *node = calloc(1, sizeof(ll_node));
   node->data = data;
@@ -31,5 +32,91 @@ add_node_by_val(ll_t *ll , void *data){
   }
   node->next = ll->head;
   ll->node_count++;
+  return 0;
+}
+
+bool_t
+is_ll_empty(ll_t *ll){
+  if(!ll) assert(0);
+  if(ll->node_count == 0)
+    return LL_TRUE;
+  return LL_FALSE;
+}
+
+int
+ll_remove_node(ll_t* ll, ll_node *node){
+  if(!ll || !ll->head ) return 0;
+  if(!node){
+    printf("%s(%d) : Error: node is NULL\n", __FUNCTION__, __LINE__);
+      return -1;
+  }
+  ll_node *head = ll->head;
+  ll_node *prev = NULL;
+  if(head == node){
+    ll->head = ll->head->next;
+    ll->node_count--;
+    node->next = NULL;
+    return 0;
+  }
+  prev = head;
+  head = head->next;
+  for(int i=1; i< ll->node_count; i++){
+    if(head != node){
+      prev = head;
+      head = head->next;
+      continue;
+    }
+    prev->next = head->next;
+    head->next = NULL;
+    ll->node_count--;
+    return 0;
+  }
+  printf("%s(%d) : Error: node is NULL\n", __FUNCTION__, __LINE__);
+  return -1;
+}
+
+int
+ll_add_node(ll_t *ll, ll_node *node){
+  if(!ll) return -1;
+  if(!node) return -1;
+  if(!ll->head){
+    ll->head = node;
+    ll->node_count++;
+    return 0;
+  }
+  node->next = ll->head;
+  ll->head = node;
+  ll->node_count++;
+  return 0;
+}
+
+int
+ll_delete_node(ll_t *ll, ll_node *node){
+  if(!ll) return -1;
+  if(!ll->head || !node) return 0;
+  ll_node *trav = NULL;
+  if(node->next){
+    ll_node *temp = NULL;
+    node->data = node->next->data;
+    temp = node->next;
+    node->next = node->next->next;
+    free(temp);
+    ll->node_count--;
+    return 0;
+  }
+  if(ll->node_count == 1 && ll->head == node){
+    free(node);
+    ll->head = NULL;
+    ll->node_count--;
+    return 0;
+  }
+  trav = ll->head;
+  while(trav->next != node){
+    trav = trav->next;
+    continue;
+  }
+  trav->next = NULL;
+  free(node);
+  ll->node_count--;
   return 0;
 }
--- simple_sample/LinkedListApi.h
+++ simple_sample/LinkedListApi.h
@@ -3,9 +3,32 @@
 
 typedef struct _ll_node ll_node;  /* opeque structure */
 typedef struct _ll_ ll_t;         /* opeque structure */
+typedef enum _bool_{
+  LL_FALSE,
+  LL_TRUE
+} bool_t;       /* opeque structure */
 
+/* function declarations */
 ll_t*
 init_singly_ll();
-int 
+int
 add_node_by_val(ll_t*, void*);
+bool_t
+is_ll_empty(ll_t*);
+int
+ll_remove_node(ll_t*, ll_node*);
+int
+ll_add_node(ll_t*, ll_node*);
+int
+ll_delete_node(ll_t*, ll_node*);
+
+/* Macros */
+#define ITERATE_LIST_BEGIN(list_ptr, node_ptr)      \
+  {                                                 \
+    ll_node *node = NULL;                           \
+    node = list_ptr->head;                          \
+    for(; node != NULL; node = node_ptr){           \
+      node = node_ptr->next;
+#define ITERATE_LIST_END(list_ptr, node_ptr) }}
+
 #endif
