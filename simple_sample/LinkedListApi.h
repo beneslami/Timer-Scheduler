6f7bc3b4dfd839fa3ef39b077476e5f13a4a874b
Benyamin <beneslami72@gmail.com>
2020-07-03 16:26:41 +0430

    change data structures to implement OO paradigm

--- simple_sample/LinkedListApi.h
+++ simple_sample/LinkedListApi.h
@@ -1,22 +1,12 @@
 #ifndef LINKEDLISTAPI_H
 #define LINKEDLISTAPI_H
 
-typedef struct _ll_node{
-  void* data;
-  struct _ll_node *next;
-}ll_node;
-
-typedef struct _ll_{
-  ll_node *head;
-  unsigned int node_count;
-  int (*comparison_fn)(void*, void*);
-  int (*order_comparison_fn)(void*, void*);
-}ll_t;
-
+typedef struct _ll_node ll_node;  /* opeque structure */
+typedef struct _ll_ ll_t;         /* opeque structure */
 typedef enum _bool_{
   LL_FALSE,
   LL_TRUE
-} bool_t;       /* opeque structure */
+} bool_t;
 
 /* function declarations */
 ll_t*
@@ -31,12 +21,24 @@ int
 ll_add_node(ll_t*, ll_node*);
 int
 ll_delete_node(ll_t*, ll_node*);
+ll_node*
+get_singly_ll_head(ll_t*);
+ll_node*
+get_next_node(ll_node*);
+ll_node*
+get_node_data(ll_node*);
+ll_t*
+get_singly_ll(ll_t*);
+void
+decrease_node_count(ll_t*);
+void
+increase_node_count(ll_t*);
 
 /* Macros */
 #define ITERATE_LIST_BEGIN(list_ptr, node_ptr)      \
   {                                                 \
     ll_node *node = NULL;                           \
-    node = list_ptr->head;                          \
+    node = get_singly_ll_head(list_ptr)             \
     for(; node != NULL; node = node_ptr){           \
       node = node_ptr->next;
 
@@ -46,9 +48,9 @@ ll_delete_node(ll_t*, ll_node*);
   {                                                             \
     ll_node *_node_ptr = NULL;                                  \
     ll_node *prev = NULL;                                       \
-    node_ptr = list_ptr->head;                                  \
+    node_ptr = get_singly_ll_head(list_ptr);                    \
     for(;node_ptr != NULL; node_ptr = _node_ptr){               \
-      _node_ptr = node_ptr->next;
+      _node_ptr = get_next_node(node_ptr);
 
 #define ITERATE_LIST_CONTINUE_ENHANCED(list_ptr, node_ptr, prev)  \
   {                                                               \
@@ -59,13 +61,13 @@ ll_delete_node(ll_t*, ll_node*);
 
 #define ITERATE_LIST_NODE_DELETE_ENHANCED(list_ptr, node_ptr, prev)   \
   {if(node_ptr && prev == NULL){                                      \
-    list_ptr->head = node_ptr_next;                                   \
+    get_singly_ll_head(list_ptr) = get_next_node(node_ptr);           \
   }                                                                   \
   else if(node_ptr && prev){                                          \
-      prev->next = node_ptr->next;                                    \
+      get_next_node(prev) = get_next_node(node_ptr);                  \
   }                                                                   \
   free(node_ptr);                                                     \
-  list_ptr->node_count--;                                             \
+  decrease_node_count(list_ptr);                                      \
   node_ptr = NULL;}
 
 #define ITERATE_LIST_END_ENHANCED(list_ptr, node_ptr, prev)      \
