c5cce4cbb6e94a40b31bf0799a85b93444e494e6
Benyamin <beneslami72@gmail.com>
2020-07-05 17:41:48 +0430

    add specific usage

--- hierarchical_timer/main.c
+++ hierarchical_timer/main.c
@@ -18,7 +18,7 @@ main(int argc, char **argv){
   start_wheel_timer(wt); /* Wheel timer has started running in a separte thread. */
 
   /* Register the events to be triggered with Wheel timer now. */
-  wheel_timer_elem_t *wt_elem = register_app_event(wt, wrapper_print, "ben_event", strlen("ben_event"), 3, 1);
+  wheel_timer_elem_t *wt_elem = register_app_event(wt, wrapper_print, "ben_event", strlen("ben_event"),12630, 1);
   wt_elem = register_app_event(wt, wrapper_print, "Oana_event", strlen("Oana_event"), 5, 1);  // special case when ben_event and Oana_event is triggered at 15th slot!
 
   scanf("\n");
